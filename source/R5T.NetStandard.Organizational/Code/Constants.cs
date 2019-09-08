507f796bc1347d2880d8cdfa669d98400e4443f8
MinexAutomation <minex.automation@gmail.com>
2019-09-08 14:41:23 -0400

    Added Data and Organizations directories.

---
+++ source/R5T.NetStandard.Organizational/Code/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.NetStandard.IO.Paths;
+using R5T.NetStandard.IO.Paths.Extensions;
+
+
+namespace R5T.NetStandard.Organizational
+{
+    public static class Constants
+    {
+        public const string DataDirectoryNameValue = "Data";
+        public static readonly DirectoryName DataDirectoryName = Constants.DataDirectoryNameValue.AsDirectoryName();
+        public const string OrganizationsDirectoryNameValue = "Organizations";
+        public static readonly DirectoryName OrganizationsDirectoryName = Constants.OrganizationsDirectoryNameValue.AsDirectoryName();
+    }
+}
